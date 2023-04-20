aa15aac8da7d7c91c5f4b80e227c905d3fcbc286
yelir <riley.c.gamer@gmail.com>
2023-04-20 10:06:09 -0400

    UI for menu screen is in progress

--- BrickBreaker/Screens/MenuScreen.Designer.cs
+++ BrickBreaker/Screens/MenuScreen.Designer.cs
@@ -30,51 +30,83 @@
         {
             this.playButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.modeSelector = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // playButton
             // 
-            this.playButton.BackColor = System.Drawing.Color.White;
+            this.playButton.BackColor = System.Drawing.Color.Yellow;
             this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.playButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
             this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
-            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.playButton.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.playButton.Location = new System.Drawing.Point(276, 238);
-            this.playButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.playButton.Font = new System.Drawing.Font("Burbank Big Cd Bk", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.ForeColor = System.Drawing.Color.Black;
+            this.playButton.Location = new System.Drawing.Point(601, 431);
             this.playButton.Name = "playButton";
-            this.playButton.Size = new System.Drawing.Size(242, 96);
+            this.playButton.Size = new System.Drawing.Size(194, 77);
             this.playButton.TabIndex = 0;
-            this.playButton.Text = "Play";
+            this.playButton.Text = "Play!";
             this.playButton.UseVisualStyleBackColor = false;
             this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            this.playButton.MouseLeave += new System.EventHandler(this.playButton_MouseLeave);
+            this.playButton.MouseHover += new System.EventHandler(this.playButton_MouseHover);
             // 
             // exitButton
             // 
-            this.exitButton.BackColor = System.Drawing.Color.White;
+            this.exitButton.BackColor = System.Drawing.Color.Yellow;
             this.exitButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.exitButton.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.exitButton.Location = new System.Drawing.Point(276, 343);
-            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Font = new System.Drawing.Font("Burbank Big Cd Bk", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(19, 433);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(242, 96);
+            this.exitButton.Size = new System.Drawing.Size(194, 77);
             this.exitButton.TabIndex = 1;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = false;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            this.exitButton.MouseLeave += new System.EventHandler(this.exitButton_MouseLeave);
+            this.exitButton.MouseHover += new System.EventHandler(this.exitButton_MouseHover);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Burbank Big Cd Bk", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(280, 17);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(274, 48);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "BRICK BREAKER\r\n";
+            // 
+            // modeSelector
+            // 
+            this.modeSelector.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.modeSelector.Font = new System.Drawing.Font("Burbank Big Cd Bk", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modeSelector.ForeColor = System.Drawing.Color.White;
+            this.modeSelector.FormattingEnabled = true;
+            this.modeSelector.Location = new System.Drawing.Point(600, 372);
+            this.modeSelector.Name = "modeSelector";
+            this.modeSelector.Size = new System.Drawing.Size(194, 35);
+            this.modeSelector.TabIndex = 3;
+            this.modeSelector.Text = "Mode";
             // 
             // MenuScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::BrickBreaker.Properties.Resources.menuScreenBackground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.modeSelector);
+            this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.playButton);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DoubleBuffered = true;
             this.Name = "MenuScreen";
-            this.Size = new System.Drawing.Size(1067, 677);
+            this.Size = new System.Drawing.Size(854, 542);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,5 +114,7 @@
 
         private System.Windows.Forms.Button playButton;
         private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.ComboBox modeSelector;
     }
 }
