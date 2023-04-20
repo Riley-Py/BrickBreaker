ef50640d9c98c468ff1f7e2d855797fd353b9dcf
Isaiah Fritch <isaifrit489@ed.amdsb.ca>
2023-04-20 09:53:21 -0400

    Isaiah Fritch - Powerup Code - V1

--- BrickBreaker/Form1.cs
+++ BrickBreaker/Form1.cs
@@ -26,9 +26,68 @@ namespace BrickBreaker
             ms.Location = new Point((this.Width - ms.Width) / 2, (this.Height - ms.Height) / 2);
         }
 
-        public static void Isaha()
+        public static void createPowerup(string powerName)
         {
+            #region Overall Notes
+            /* Code written by Isaha Flinch.
+             * This code exists to create powerups for player to use.  The powerups
+             * are stored with the data of a block and are generated upon that block's 
+             * distruction.  The powerup will fall towards the player slowly.  If 
+             * collected, the powerup will be given.  Otherwise the powerup will be 
+             * removed without use.
+             */
+            #endregion
 
+            switch (powerName)
+            {
+                //add ball
+                case "Ammo":
+                    #region notes
+                    //add a ball to the game
+                    //store the ball within a list
+                    //only take a life from the player if the balls in that list = 0
+                    #endregion
+                    break;
+
+                //add extra life, to a maximum of one extra life
+                case "ChugJug":
+                    #region notes
+                    //check the lives count
+                    //if the default is 3 lives than we can add another until it reaches 4 lives
+                    //otherwise do nothing
+                    #endregion
+                    break;
+
+                //temporary machine gun that fires straight upwards
+                case "Scar":
+                    #region notes
+                    //create a gun class that stores which of the three guns are in use
+                    //have that gun class launch projectiles upwards
+                    //have code within the game that calls a move method for the gun bullets (so have a class for each type of bullet)
+                    //when the bullets collide with the blocks, have them remove a life from the block
+                    //have the weapon disappear after a few seconds
+
+                    //gun class(gun type), bullet class(bullet type(scar,shot,rpg))
+                    //gun class makes gun, which makes the bullets 
+                    #endregion
+                    break;
+
+                //temporary shotgun that fires straight upwards
+                case "Shotgun":
+                    break;
+
+                //rocket launcher that fires straight upwards once, AOE attack
+                case "RocketLauncher":
+                    break;
+
+                //reduce half of the lives of each block
+                case "InfinityGauntlet ":
+                    #region notes
+                    //for each block, divide it's lives by two (round up on odd numbers)
+                    //to make up for the rounding, any block with one life will be destroyed
+                    #endregion
+                    break;
+            }       
         }
     }
 }
