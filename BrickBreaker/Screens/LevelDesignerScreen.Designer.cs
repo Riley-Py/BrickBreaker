852c563a02165699722086426bdb3ffaaf3066da
Brayden Nykamp <braynyka385@ed.amdsb.ca>
2023-04-27 09:58:09 -0400

    Bruh we did it

--- BrickBreaker/Screens/LevelDesignerScreen.Designer.cs
+++ BrickBreaker/Screens/LevelDesignerScreen.Designer.cs
@@ -36,28 +36,30 @@
             // powerUpLabel
             // 
             this.powerUpLabel.AutoSize = true;
-            this.powerUpLabel.Location = new System.Drawing.Point(385, 492);
-            this.powerUpLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.powerUpLabel.Location = new System.Drawing.Point(578, 757);
             this.powerUpLabel.Name = "powerUpLabel";
-            this.powerUpLabel.Size = new System.Drawing.Size(33, 13);
+            this.powerUpLabel.Size = new System.Drawing.Size(47, 20);
             this.powerUpLabel.TabIndex = 0;
             this.powerUpLabel.Text = "None";
             // 
             // instructionLabel
             // 
             this.instructionLabel.AutoSize = true;
-            this.instructionLabel.Font = new System.Drawing.Font("Burbank Big Cd Bk", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.instructionLabel.ForeColor = System.Drawing.Color.White;
-            this.instructionLabel.Location = new System.Drawing.Point(3, 114);
+            this.instructionLabel.Location = new System.Drawing.Point(4, 175);
+            this.instructionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.instructionLabel.Name = "instructionLabel";
-            this.instructionLabel.Size = new System.Drawing.Size(101, 21);
+            this.instructionLabel.Size = new System.Drawing.Size(192, 37);
             this.instructionLabel.TabIndex = 1;
             this.instructionLabel.Text = "instructions";
             this.instructionLabel.Visible = false;
+            // 
             // replaceLabel
             // 
             this.replaceLabel.AutoSize = true;
-            this.replaceLabel.Location = new System.Drawing.Point(838, 40);
+            this.replaceLabel.Location = new System.Drawing.Point(871, 73);
+            this.replaceLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.replaceLabel.Name = "replaceLabel";
             this.replaceLabel.Size = new System.Drawing.Size(99, 20);
             this.replaceLabel.TabIndex = 1;
@@ -65,16 +67,14 @@
             // 
             // LevelDesignerScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RosyBrown;
             this.Controls.Add(this.instructionLabel);
-
             this.Controls.Add(this.replaceLabel);
             this.Controls.Add(this.powerUpLabel);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LevelDesignerScreen";
-            this.Size = new System.Drawing.Size(853, 528);
+            this.Size = new System.Drawing.Size(1280, 812);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.LevelDesignerScreen_Paint);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.LevelDesignerScreen_KeyUp);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.LevelDesignerScreen_MouseClick);
