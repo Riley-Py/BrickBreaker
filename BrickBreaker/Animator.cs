c357866226562c9fc6dfbca6cd88b7533c908f1a
Brayden Nykamp <braynyka385@ed.amdsb.ca>
2023-05-02 09:38:24 -0400

    Start of animator

---
+++ BrickBreaker/Animator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickBreaker
+{
+    internal class Animator
+    {
+        int x, y;
+
+        int frameCount;
+        int duration;
+        AnimationStyle animationStyle;
+
+        public Animator(int x, int y, int frameCount, Color c)
+        {
+
+        }
+        public Animator(int x, int y, int frameCount, Bitmap[] bitmaps)
+        {
+
+        }
+    }
+
+    enum AnimationStyle
+    {
+        Bitmap = 0,
+        Particle = 1
+    }
+}
